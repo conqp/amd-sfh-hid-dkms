cef7be590da0b6600cc7d8f7150ba8cc808197d0
Richard Neumann <mail@richard-neumann.de>
2021-01-25 03:49:57 +0100

    Added missing pci_dev to pci_ message calls.

--- amd-sfh-quirks.c
+++ amd-sfh-quirks.c
@@ -42,10 +42,10 @@ static const struct dmi_system_id hp_envy_x360[] = {
  */
 uint amd_sfh_quirks_get_sensor_mask(struct pci_dev *pci_dev) {
 	if (dmi_check_system(hp_envy_x360)) {
-		pci_info("Detected HP ENVY x360 series convertible.\n");
+		pci_info(pci_dev, "Detected HP ENVY x360 series convertible.\n");
 		return ACCEL_MASK + MAGNO_MASK;
 	}
 
-	pci_warn("No quirks available for this hardware.\n");
+	pci_warn(pci_dev, "No quirks available for this hardware.\n");
 	return 0;
 }
