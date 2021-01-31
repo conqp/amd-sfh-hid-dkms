827391c3716038498c55d333307441ace3353df6
Richard Neumann <mail@richard-neumann.de>
2021-01-31 21:45:11 +0100

    Removed debug printing.

--- amd-sfh-hid-ll-drv.c
+++ amd-sfh-hid-ll-drv.c
@@ -76,7 +76,6 @@ static int amd_sfh_hid_ll_parse(struct hid_device *hid)
 		return -EINVAL;
 	}
 
-	hid_err(hid, "Report size: %ld", size);
 	rc = hid_parse_report(hid, buf, size);
 	kfree(buf);
 	return rc;
