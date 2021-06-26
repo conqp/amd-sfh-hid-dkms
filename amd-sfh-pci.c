e25649ac7705f90874cbdfd08312bbaa2db67b64
Richard Neumann <mail@richard-neumann.de>
2021-06-26 15:12:46 +0200

    Use pci_set_consistent_dma_mask()

--- amd-sfh-pci.c
+++ amd-sfh-pci.c
@@ -224,9 +224,9 @@ static int amd_sfh_pci_probe(struct pci_dev *pci_dev,
 
 	privdata->mmio = pcim_iomap_table(pci_dev)[2];
 	pci_set_master(pci_dev);
-	rc = pci_set_dma_mask(pci_dev, DMA_BIT_MASK(64));
+	rc = pci_set_consistent_dma_mask(pci_dev, DMA_BIT_MASK(64));
 	if (rc)
-		rc = pci_set_dma_mask(pci_dev, DMA_BIT_MASK(32));
+		rc = pci_set_consistent_dma_mask(pci_dev, DMA_BIT_MASK(32));
 	if (rc)
 		return rc;
 
